2e0ffc0676b023e792f71f7534c77192f43564d7
tatimblin <tatimblin@gmail.com>
2020-12-02 09:25:59 -0500

    day1: read input.txt

---
+++ day1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	lines, err := readLines("input.txt")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+	for i, line := range lines {
+		fmt.Println(i, line)
+	}
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
