9b1bbc303343f996ddf02e057af0323a0ebd8167
tatimblin <tatimblin@gmail.com>
2020-12-02 23:03:11 -0500

    get numbers

--- day1/main.go
+++ day1/main.go
@@ -2,19 +2,50 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
+type Numbers struct {
+	First  int
+	Second int
+}
+
 func main() {
+	var numbers []int
+
 	lines, err := readLines("input.txt")
 	if err != nil {
 		log.Fatalf("readLines: %s", err)
 	}
-	for i, line := range lines {
-		fmt.Println(i, line)
+	for _, line := range lines {
+		val, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatalf("string convert: %s", err)
+		}
+		numbers = append(numbers, val)
 	}
+
+	match := checkSum(numbers, 2020)
+	if err != nil {
+		log.Fatalf("Sum match not found")
+	}
+	log.Println(match)
+}
+
+func checkSum(numbers []int, sum int) Numbers {
+	for i, number := range numbers {
+		for _, otherNumber := range numbers[i:] {
+			if number+otherNumber == sum {
+				return Numbers{
+					First:  number,
+					Second: otherNumber,
+				}
+			}
+		}
+	}
+	return Numbers{}
 }
 
 func readLines(path string) ([]string, error) {
